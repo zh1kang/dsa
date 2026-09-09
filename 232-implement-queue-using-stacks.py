b4c25017b915a25814869ecbb7104f142dcfccaf
Caleb <uongcaleb@gmail.com>
2026-09-08 20:22:49 -0400

    leetcode: Implement Queue using Stacks

--- 232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks.py
@@ -78,3 +78,53 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+# submission 2134051479 - 2026-09-07T15:25:12+00:00
+class MyQueue:
+
+    def __init__(self):
+        self.input = []
+        self.output = []
+       
+
+        
+
+    def push(self, x: int) -> None:
+        self.input.append(x)
+        
+
+    def pop(self) -> int:
+        # we want to pop the top, but since stacks are LIFO, we reverse it by appending everything in input to ouput 
+        
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+
+        return self.output.pop()
+        
+        
+
+    def peek(self) -> int:
+
+        # peek checks the element in the front of the queue; so we do the same thing and return self.output[-1]
+
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+
+        return self.output[-1]
+       
+
+    def empty(self) -> bool:
+        return not self.input and not self.output
+        
+# divergences:
+# none
+# stacks are LIFO and queues are FIFO so appending all items in one stack to another reverses this and gives us the proper behavior we want for a queue 
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
