61e1f14305d44fa09131b936a42a48469baaf89c
Caleb <uongcaleb@gmail.com>
2026-09-08 20:22:49 -0400

    leetcode: Top K Frequent Elements

--- 347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements.py
@@ -29,3 +29,38 @@ class Solution:
 
 
         
+
+# submission 2134062696 - 2026-09-07T15:35:01+00:00
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # create a counter / freq array for the numbers in num
+        # initialize a heap
+        # push if a number has a greater freq
+        # only keep k amount in heap
+        # return res
+
+
+        count = Counter(nums)
+        res = []
+        heap = []
+
+        for num, freq in count.items():
+            # initialize heap
+            heapq.heappush(heap, (freq, num))
+
+            # if we have more than k elemtns in the heap pop the top since it will be the least frequent number
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        # go through the heap and append it to res    
+        
+        for _, num in heap:
+            res.append(num)
+
+        return res
+
+
+
+            
+        
+       
