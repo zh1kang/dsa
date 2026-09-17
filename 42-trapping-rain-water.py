529c5e8d5164ea0ce7b23a40a3d3f1171afe43dd
Caleb <uongcaleb@gmail.com>
2026-09-16 22:23:58 -0400

    leetcode: Trapping Rain Water

--- 42-trapping-rain-water.py
+++ 42-trapping-rain-water.py
@@ -34,3 +34,41 @@ class Solution:
                 maxTrap += rightMax - height[right]
 
         return maxTrap 
+
+# submission 2138084445 - 2026-09-11T03:16:55+00:00
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        if not height:
+            return 0 
+
+        stack = []
+        water = 0
+
+        for i, curr_height in enumerate(height):
+
+            # if the current height is taller than the height on top of the stack, we have found a "right" side wall
+            # that can hold the water
+
+            while stack and curr_height > height[stack[-1]]:
+                floor = stack.pop()
+
+                # if stack is empty after we pop the top, that means there is no left wall to hold the water
+                if not stack:
+                    break
+
+                left_idx = stack[-1]
+
+                width = i - left_idx - 1
+
+                bound_height = min(curr_height, height[left_idx]) - height[floor]
+            
+                water += width * bound_height
+
+            stack.append(i)
+
+        return water
+
+
+
+        
