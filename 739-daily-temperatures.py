1a06d5a85f9cc99d90710c1a7f4c041a1bcf349e
Caleb <uongcaleb@gmail.com>
2026-09-16 22:23:58 -0400

    leetcode: Daily Temperatures

--- 739-daily-temperatures.py
+++ 739-daily-temperatures.py
@@ -60,3 +60,24 @@ class Solution:
 
 
         
+
+# submission 2141032424 - 2026-09-13T21:42:47+00:00
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+
+        res = [0] * len(temperatures)
+        stack = []
+
+
+        for idx, temp in enumerate(temperatures):
+
+            while stack and temperatures[stack[-1]] < temp:
+                i = stack.pop()
+                res[i] = idx - i
+
+            stack.append(idx)
+
+        
+        return res 
+
+        
