8e987dd4f50c55ae652894365b6a61159cabe229
Caleb <uongcaleb@gmail.com>
2026-09-16 22:23:58 -0400

    leetcode: Length of Last Word

---
+++ 58-length-of-last-word.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        last_word = s.split()[-1]
+
+
+        eturn len(last_word)
+
+# submission 2141808974 - 2026-09-14T17:06:58+00:00
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        last_word = s.split()[-1]
+
+
+        return len(last_word)
