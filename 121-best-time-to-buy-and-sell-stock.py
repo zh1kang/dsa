fde3524fd62bd13671d463cdf3088a4f2567a4b1
Caleb <uongcaleb@gmail.com>
2026-09-22 22:46:41 -0400

    leetcode: Best Time to Buy and Sell Stock

--- 121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock.py
@@ -48,3 +48,60 @@ class Solution:
             r += 1
         return maxx 
         
+
+# submission 2146196080 - 2026-09-19T00:51:02+00:00
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        max_profit = 0
+        l, r = 0, 1
+
+        while r < len(prices):
+            profit = 0 
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+            else: 
+                l = r
+            r += 1
+
+        return max_profit
+
+            
+                
+
+# submission 2146196867 - 2026-09-19T00:54:27+00:00
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        # we can greedy and always sell when the price is smaller than min price
+
+        min_price = float('inf')
+        max_profit = 0
+
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+             
+
+        return max_profit 
+
+# submission 2146197294 - 2026-09-19T00:56:31+00:00
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        # we can greedy by keeping track of the min price every day
+        # assuming that today could be the best day to sell 
+
+        min_price = float('inf')
+        max_profit = 0
+
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+             
+
+        return max_profit 
