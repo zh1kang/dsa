3face0b828147f63256f542f163aaea35f40af4a
Caleb <uongcaleb@gmail.com>
2026-09-22 22:46:41 -0400

    leetcode: Valid Parentheses

--- 20-valid-parentheses.py
+++ 20-valid-parentheses.py
@@ -18,3 +18,24 @@ class Solution:
         
 
         
+
+# submission 2146198835 - 2026-09-19T01:04:06+00:00
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        valid = {")" : "(", "}" : "{", "]" : "["}
+
+
+        for char in s:
+            # append the opening parenthesis 
+            if char in valid.values():
+                stack.append(char)
+            # if the char is a closing parenthesis we check if the top of the stack has the matching parenthesis
+            elif char in valid.keys():
+                if not stack or valid[char] != stack.pop():
+                    return False
+        
+        return not stack  
+
+        
