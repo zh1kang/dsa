f15a163d03b60687706c8b8f7370bb4909509d77
Caleb <uongcaleb@gmail.com>
2026-09-22 22:46:41 -0400

    leetcode: Contains Duplicate

--- 217-contains-duplicate.py
+++ 217-contains-duplicate.py
@@ -13,3 +13,22 @@ class Solution:
 
             
     
+
+# submission 2149172935 - 2026-09-22T00:24:44+00:00
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        num_set = set()
+
+        for num in nums:
+            if num in num_set:
+                return True
+
+            num_set.add(num)
+
+        return False 
+
+
+
+            
+    
